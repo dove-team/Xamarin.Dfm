7f4891c8f334f3e8b7dac85f15a82bcdb2523a2e
unknown <michael19950415@outlook.com>
2020-08-19 10:17:52 +0800

    upgrade

---
+++ DanmakuParser.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using Xamarin.Dfm.Extensions;
+using Xamarin.Dfm.Model;
+
+namespace Xamarin.Dfm
+{
+    public sealed class DanmakuParser
+    {
+        private HttpClientEx HttpClient { get; }
+        public DanmakuParser()
+        {
+            HttpClient = new HttpClientEx();
+        }
+        public async Task<List<DanmakuModel>> ParseBiliBili(long cid)
+        {
+            try
+            {
+                string danmuStr = await HttpClient.GetResultsDeflate(new Uri(string.Format("https://api.bilibili.com/x/v1/dm/list.so?oid={0}", cid)));
+                return ParseBiliBiliXml(danmuStr);
+            }
+            catch (Exception ex)
+            {
+                LogManager.Instance.LogError("ParseBiliBili", ex);
+                return null;
+            }
+        }
+        public List<DanmakuModel> ParseBiliBili(string xml)
+        {
+            return ParseBiliBiliXml(xml);
+        }
+        private List<DanmakuModel> ParseBiliBiliXml(string xmlStr)
+        {
+            List<DanmakuModel> danmakus = new List<DanmakuModel>();
+            try
+            {
+                XmlDocument xdoc = new XmlDocument();
+                xmlStr = Regex.Replace(xmlStr, @"[\x00-\x08]|[\x0B-\x0C]|[\x0E-\x1F]", "");
+                xdoc.LoadXml(xmlStr);
+                foreach (XmlNode item in xdoc.DocumentElement.ChildNodes)
+                {
+                    if (item.Attributes["p"] != null)
+                    {
+                        try
+                        {
+                            string node = item.Attributes["p"].Value;
+                            string[] danmaku = node.Split(',');
+                            var location = danmaku[1] switch
+                            {
+                                "7" => DanmakuLocation.Position,
+                                "4" => DanmakuLocation.Bottom,
+                                "5" => DanmakuLocation.Top,
+                                _ => DanmakuLocation.Roll,
+                            };
+                            danmakus.Add(new DanmakuModel
+                            {
+                                time = double.Parse(danmaku[0]),
+                                time_s = Convert.ToInt32(double.Parse(danmaku[0])),
+                                location = location,
+                                size = double.Parse(danmaku[2]),
+                                color = danmaku[3].ToColor(true),
+                                sendTime = danmaku[4],
+                                pool = danmaku[5],
+                                sendID = danmaku[6],
+                                rowID = danmaku[7],
+                                text = item.InnerText,
+                                source = item.OuterXml,
+                                fromSite = DanmakuSite.Bilibili
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            LogManager.Instance.LogError("ParseBiliBiliXml", ex);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogManager.Instance.LogError("ParseBiliBiliXml", ex);
+            }
+            return danmakus;
+        }
+    }
+}
---
+++ Extensions/HttpClientEx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Android.Net;
+
+namespace Xamarin.Dfm.Extensions
+{
+    public sealed class HttpClientEx
+    {
+        public async Task<string> GetResultsDeflate(Uri url)
+        {
+            try
+            {
+                var clientHandler = new AndroidClientHandler { UseCookies = true };
+                clientHandler.ServerCertificateCustomValidationCallback += (sender, cert, chaun, ssl) => { return true; };
+                using HttpClient hc = new HttpClient(clientHandler);
+                HttpResponseMessage hr = await hc.GetAsync(url).ConfigureAwait(false);
+                hr.EnsureSuccessStatusCode();
+                var encodeResults = await hr.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                DeflateStream deflateStream = new DeflateStream(encodeResults, CompressionMode.Decompress);
+                StreamReader streamReader = new StreamReader(deflateStream, Encoding.UTF8);
+                return streamReader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                LogManager.Instance.LogError("GetResultsDeflate", ex);
+                return string.Empty;
+            }
+        }
+    }
+}
